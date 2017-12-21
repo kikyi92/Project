b71081daf47bbeb840c4b04964317143f014f509
Xiness <Xiness@192.168.0.39>
2017-12-21 13:37:16 +0900

    쿼리에 있는 이름으로 출력한 값을 ajax_call2 에 출력하기.

--- Project/src/main/java/com/mycompany/myapp/HomeController.java
+++ Project/src/main/java/com/mycompany/myapp/HomeController.java
@@ -3,9 +3,6 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.mycompany.dto.UserListResponseDTO;
-import com.mycompany.service.UserListService; 
 import com.mycompany.service.UserListServiceImpl;
 
 /** * Handles requests for the application home page. */
@@ -39,7 +35,23 @@ public class HomeController {
 		model.addAttribute("serverTime", formattedDate);
 		return "home";
 	}
-
+	//TE
+	@RequestMapping(value = "/ajax_call2.do", method = RequestMethod.POST) //do 
+	public ModelAndView ajax_call2(ModelAndView mv) {
+		// Service call2
+		List<UserListResponseDTO> userlist = userListService.getUserListService1();
+		for (int i = 0; i < userlist.size(); i++) {
+			System.out.print("이름: " + userlist.get(i).getUsername());
+			System.out.print("성별: " + userlist.get(i).getUserGender());
+			System.out.print("나이: " + userlist.get(i).getUserAge());
+			System.out.println("핸드폰번호: " + userlist.get(i).getUserPhone());
+		}
+		mv.addObject("listuser", userlist);
+		mv.setViewName("ajax_call2");
+		return mv;
+	}
+	//ST
+	
 	@RequestMapping(value = "/DevelopmentTeam.do", method = RequestMethod.GET)
 	public ModelAndView DevelopmentTeam(ModelAndView mv) {
 		// Service call
--- Project/src/main/webapp/WEB-INF/views/home.jsp
+++ Project/src/main/webapp/WEB-INF/views/home.jsp
@@ -1,20 +1,43 @@
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
-<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
 
-<%@ page session="false" %>
+<%@ page session="false"%>
 <html>
 <head>
-	<title>Home</title>
+<title>자이네스</title>
+
+<!-- Test  -->
+<script src="http://code.jquery.com/jquery-3.1.0.min.js"></script>
+<script>
+$(document).ready(function(){
+    $("#val").click(function(){
+    $.ajax({
+        type: "post",
+        url: 'ajax_call.jsp',
+        success : function test(a){ $("#callback").html(a); },
+         error : function error(){alert("error");} 
+        });
+    });
+});
+</script>
+<!-- TE  -->
 </head>
-<body> 
-<h1>
-	Xiness
-</h1>
+<body>
+	<h1>Xiness</h1>
+	<!-- Test  -->
+	<input type="button" id="val" value="개발팀 팀원 ">
+	<div id="callback"></div>
+<!-- ST  -->
 
- <input type="button" value="개발팀 정보" onClick="location.href='DevelopmentTeam.do'"> 
-
-<!--  <li class="">
-  <a href="DevelopmentTeam.do">개발팀</a>
- </li> -->
+	<div>
+		<input type="button" value="개발팀 모든정보"
+			onClick="location.href='DevelopmentTeam.do'">
+	</div>
+	
 </body>
 </html>
+
+	<!--  <li class="">
+  <a href="DevelopmentTeam.do">개발팀</a>
+ </li> -->
